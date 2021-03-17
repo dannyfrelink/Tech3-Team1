cbd5463cbbc8e6d92f480f2ca76b59ced04e065c
dannyfrelink <danny-frelink@hotmail.com>
2021-03-17 11:07:10 +0100

    toevoeging eslint & aanpassingen quotes

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-const express = require(`express`);
-const expHandlebars = require(`express-handlebars`);
+const express = require('express');
+const expHandlebars = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 5555;
-const multer = require(`multer`);
-const { v4: uuidv4 } = require(`uuid`);
-const countriesList = require(`countries-list`);
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+const countriesList = require('countries-list');
 /* eslint-disable-next-line no-unused-vars */
-const bodyParser = require(`body-parser`);
+const bodyParser = require('body-parser');
 /* eslint-disable-next-line no-unused-vars */
-const FileReader = require(`filereader`);
+const FileReader = require('filereader');
 /* eslint-disable-next-line no-unused-vars */
-const dotenv = require(`dotenv`).config();
-const { MongoClient } = require(`mongodb`);
+const dotenv = require('dotenv').config();
+const { MongoClient } = require('mongodb');
 
 const dbURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URI}`;
 
@@ -44,19 +44,19 @@ const options = {
 
 MongoClient.connect(dbURL, { useUnifiedTopology: true }, (err, client) => {
 	if (err) {
-		console.log(`MongoDB Error:` + err);
+		console.log('MongoDB Error:' + err);
 	} else {
 		db = client.db(process.env.DB_NAME);
-		users = db.collection(`persoonsgegevens`);
-		photos = db.collection(`fotos`);
-		search = db.collection(`zoekopdracht`);
-		travel = db.collection(`landen`);
+		users = db.collection('persoonsgegevens');
+		photos = db.collection('fotos');
+		search = db.collection('zoekopdracht');
+		travel = db.collection('landen');
 	}
 });
 
 const storage = multer.diskStorage({
 	destination: (req, file, callback) => {
-		callback(null, `./static/public/uploads/`);
+		callback(null, './static/public/uploads/');
 	},
 	filename: (req, file, callback) => {
 		callback(null, `${uuidv4(options.random)}.jpg`);
@@ -77,59 +77,59 @@ const hbs = expHandlebars.create({
 });
 
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static(`static/public`));
+app.use(express.static('static/public'));
 
-app.engine(`handlebars`, hbs.engine);
-app.set(`view engine`, `handlebars`);
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
-app.get(`/persoonsgegevens`, (req, res) => {
-	res.render(`profiel-1`, { countries });
+app.get('/persoonsgegevens', (req, res) => {
+	res.render('profiel-1', { countries });
 });
 
-app.post(`/fotos`, async (req, res) => {
+app.post('/fotos', async (req, res) => {
 	try {
-		const document = { "name": req.body.name, "nationaliteit": req.body.nation, "geboortedatum": req.body.geboorte, "geslacht": req.body.geslacht, "bio": req.body.bio };
+		const document = { 'name': req.body.name, 'nationaliteit': req.body.nation, 'geboortedatum': req.body.geboorte, 'geslacht': req.body.geslacht, 'bio': req.body.bio };
 		await users.insertOne({ document });
 	}
 	catch (error) {
-		console.error(`Error:`, error);
+		console.error('Error:', error);
 	}
 
-	res.render(`addFoto`);
+	res.render('addFoto');
 });
 
-app.post(`/profiel/fotos`, upload.array(`fotos`), async (req, res) => {
+app.post('/profiel/fotos', upload.array('fotos'), async (req, res) => {
 	let paths = [];
-	paths = req.files.map(file => file.path.split(`/`).pop());
+	paths = req.files.map(file => file.path.split('/').pop());
 
 	let usersDB = {};
 	let photosDB = {};
 
 	try {
-		const document = { "pfImage": paths[0], "extraImages": paths.slice(1)};
+		const document = { 'pfImage': paths[0], 'extraImages': paths.slice(1)};
 		await photos.insertOne({ document });
 
 		usersDB = await users.findOne({}, { sort: { _id: -1 }, limit: 1 });
 		photosDB = await photos.findOne({}, { sort: { _id: -1 }, limit: 1 });
 	}
 	catch (error) {
-		console.error(`Error:`, error);
+		console.error('Error:', error);
 	}
 
-	res.render(`profiel-2`, { usersDB, photosDB });
+	res.render('profiel-2', { usersDB, photosDB });
 });
 
-app.get(`/zoekopdracht`, async (req, res) => {
-	res.render(`addZoekopdracht`, { countries});
+app.get('/zoekopdracht', async (req, res) => {
+	res.render('addZoekopdracht', { countries});
 });
 
-app.post(`/profiel/zoekopdracht`, async (req, res) => {
+app.post('/profiel/zoekopdracht', async (req, res) => {
 	let usersDB = {};
 	let photosDB = {};
 	let searchDB = {};
 
 	try {
-		const document = { "geslacht": req.body.geslacht, "nationaliteit": req.body.nation, "leeftijd": req.body.leeftijd, "interesses": req.body.interesses };
+		const document = { 'geslacht': req.body.geslacht, 'nationaliteit': req.body.nation, 'leeftijd': req.body.leeftijd, 'interesses': req.body.interesses };
 		await search.insertOne({ document });
 
 		usersDB = await users.findOne({}, { sort: { _id: -1 }, limit: 1 });
@@ -137,23 +137,23 @@ app.post(`/profiel/zoekopdracht`, async (req, res) => {
 		searchDB = await search.findOne({}, { sort: { _id: -1 }, limit: 1 });
 	}
 	catch (error) {
-		console.error(`Connectie mislukt`, error);
+		console.error('Connectie mislukt', error);
 	}
-	res.render(`profiel-3`, { usersDB, photosDB, searchDB });
+	res.render('profiel-3', { usersDB, photosDB, searchDB });
 });
 
-app.get(`/reizen`, async (req, res) => {
-	res.render(`addReizen`, { continents, countries, hbs });
+app.get('/reizen', async (req, res) => {
+	res.render('addReizen', { continents, countries, hbs });
 });
 
-app.post(`/profiel/reizen`, async (req, res) => {
+app.post('/profiel/reizen', async (req, res) => {
 	let usersDB = {};
 	let photosDB = {};
 	let searchDB = {};
 	let countriesDB = {};
 
 	try {
-		const document = { "landen": req.body.landen };
+		const document = { 'landen': req.body.landen };
 		await travel.insertOne(document);
 
 		usersDB = await users.findOne({}, { sort: { _id: -1 }, limit: 1 });
@@ -162,14 +162,14 @@ app.post(`/profiel/reizen`, async (req, res) => {
 		countriesDB = await travel.findOne({}, { sort: { _id: -1 }, limit: 1 });
 	}
 	catch (error) {
-		console.error(`Connectie mislukt`, error);
+		console.error('Connectie mislukt', error);
 	}
-	res.render(`profiel-4`, { countries, usersDB, photosDB, searchDB, countriesDB });
+	res.render('profiel-4', { countries, usersDB, photosDB, searchDB, countriesDB });
 });
 
 
 app.use(function (req, res) {
-	res.status(404).send(`Sorry, deze pagina kon ik niet vinden.`);
+	res.status(404).send('Sorry, deze pagina kon ik niet vinden.');
 });
 
 app.listen(port, () => {
--- static/public/js/script.js
+++ static/public/js/script.js
@@ -1,22 +1,22 @@
 /* eslint-disable no-undef */
 
-const chooseSelects = document.querySelectorAll(`form.zoekopdracht select`);
-const geenVoorkeurInputs = document.querySelectorAll(`form.zoekopdracht>div:not(:last-of-type) input[type="checkbox"]`);
+const chooseSelects = document.querySelectorAll('form.zoekopdracht select');
+const geenVoorkeurInputs = document.querySelectorAll('form.zoekopdracht>div:not(:last-of-type) input[type="checkbox"]');
 
-const imageInputs = document.querySelectorAll(`form.fotos input`);
-const imageLabels = document.querySelectorAll(`form.fotos label`);
+const imageInputs = document.querySelectorAll('form.fotos input');
+const imageLabels = document.querySelectorAll('form.fotos label');
 
-const continenten = document.querySelectorAll(`form.reizen>div:first-of-type label`);
-const landenContinenten = document.querySelectorAll(`form.reizen>div:nth-of-type(2) div`);
+const continenten = document.querySelectorAll('form.reizen>div:first-of-type label');
+const landenContinenten = document.querySelectorAll('form.reizen>div:nth-of-type(2) div');
 
 chooseSelects.forEach(function (chooseSelect) {
-	chooseSelect.addEventListener(`change`, function () {
-		chooseSelect.classList.toggle(`checked`);
+	chooseSelect.addEventListener('change', function () {
+		chooseSelect.classList.toggle('checked');
 	});
 });
 
 imageInputs.forEach(function (imageInput) {
-	imageInput.addEventListener(`change`, function (e) {
+	imageInput.addEventListener('change', function (e) {
 		displayImage(e);
 		displayLabel();
 	});
@@ -27,11 +27,11 @@ function displayImage(e) {
 
 	const fileReader = new FileReader();
 	fileReader.readAsDataURL(e.target.files[0]);
-	fileReader.addEventListener(`load`, function () {
+	fileReader.addEventListener('load', function () {
 		labelElement.innerHTML = `<img src="${this.result}" />`;
 	});
 
-	e.target.addEventListener(`click`, function (e) {
+	e.target.addEventListener('click', function (e) {
 		e.preventDefault();
 	});
 }
@@ -41,72 +41,72 @@ function displayLabel() {
 
 	imageLabels.forEach(function (imageLabel, index) {
 		imageLabel.childNodes.forEach(function (childNode) {
-			if (childNode.tagName !== `IMG` && !found) {
+			if (childNode.tagName !== 'IMG' && !found) {
 				found = true;
-				imageLabels[index + 1].style.display = `block`;
+				imageLabels[index + 1].style.display = 'block';
 			}
 		});
 	});
 }
 
 for (let i = 0; i < landenContinenten.length; i++) {
-	continenten[0].addEventListener(`click`, function () {
-		continenten[i].classList.remove(`checked`);
-		continenten[0].classList.add(`checked`);
-		landenContinenten[i].style.display = `none`;
-		landenContinenten[0].style.display = `flex`;
+	continenten[0].addEventListener('click', function () {
+		continenten[i].classList.remove('checked');
+		continenten[0].classList.add('checked');
+		landenContinenten[i].style.display = 'none';
+		landenContinenten[0].style.display = 'flex';
 	});
 
-	continenten[1].addEventListener(`click`, function () {
-		continenten[i].classList.remove(`checked`);
-		continenten[1].classList.add(`checked`);
-		landenContinenten[i].style.display = `none`;
-		landenContinenten[1].style.display = `flex`;
+	continenten[1].addEventListener('click', function () {
+		continenten[i].classList.remove('checked');
+		continenten[1].classList.add('checked');
+		landenContinenten[i].style.display = 'none';
+		landenContinenten[1].style.display = 'flex';
 	});
 
-	continenten[2].addEventListener(`click`, function () {
-		continenten[i].classList.remove(`checked`);
-		continenten[2].classList.add(`checked`);
-		landenContinenten[i].style.display = `none`;
-		landenContinenten[2].style.display = `flex`;
+	continenten[2].addEventListener('click', function () {
+		continenten[i].classList.remove('checked');
+		continenten[2].classList.add('checked');
+		landenContinenten[i].style.display = 'none';
+		landenContinenten[2].style.display = 'flex';
 	});
 
-	continenten[3].addEventListener(`click`, function () {
-		continenten[i].classList.remove(`checked`);
-		continenten[3].classList.add(`checked`);
-		landenContinenten[i].style.display = `none`;
-		landenContinenten[3].style.display = `flex`;
+	continenten[3].addEventListener('click', function () {
+		continenten[i].classList.remove('checked');
+		continenten[3].classList.add('checked');
+		landenContinenten[i].style.display = 'none';
+		landenContinenten[3].style.display = 'flex';
 	});
 
-	continenten[4].addEventListener(`click`, function () {
-		continenten[i].classList.remove(`checked`);
-		continenten[4].classList.add(`checked`);
-		landenContinenten[i].style.display = `none`;
-		landenContinenten[4].style.display = `flex`;
+	continenten[4].addEventListener('click', function () {
+		continenten[i].classList.remove('checked');
+		continenten[4].classList.add('checked');
+		landenContinenten[i].style.display = 'none';
+		landenContinenten[4].style.display = 'flex';
 	});
 
-	continenten[5].addEventListener(`click`, function () {
-		continenten[i].classList.remove(`checked`);
-		continenten[5].classList.add(`checked`);
-		landenContinenten[i].style.display = `none`;
-		landenContinenten[5].style.display = `flex`;
+	continenten[5].addEventListener('click', function () {
+		continenten[i].classList.remove('checked');
+		continenten[5].classList.add('checked');
+		landenContinenten[i].style.display = 'none';
+		landenContinenten[5].style.display = 'flex';
 	});
 
-	continenten[6].addEventListener(`click`, function () {
-		continenten[i].classList.remove(`checked`);
-		continenten[6].classList.add(`checked`);
-		landenContinenten[i].style.display = `none`;
-		landenContinenten[6].style.display = `flex`;
+	continenten[6].addEventListener('click', function () {
+		continenten[i].classList.remove('checked');
+		continenten[6].classList.add('checked');
+		landenContinenten[i].style.display = 'none';
+		landenContinenten[6].style.display = 'flex';
 	});
 }
 
 if (geenVoorkeurInputs[0]) {
-	geenVoorkeurInputs[0].addEventListener(`click`, function () {
-		chooseSelects[0].toggleAttribute(`disabled`);
+	geenVoorkeurInputs[0].addEventListener('click', function () {
+		chooseSelects[0].toggleAttribute('disabled');
 	});
 }
 if (geenVoorkeurInputs[1]) {
-	geenVoorkeurInputs[1].addEventListener(`click`, function () {
-		chooseSelects[1].toggleAttribute(`disabled`);
+	geenVoorkeurInputs[1].addEventListener('click', function () {
+		chooseSelects[1].toggleAttribute('disabled');
 	});
 }
