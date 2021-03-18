2bd30d3cec2c6650d31a7b2cbb269ec2e2fde95b
KathleenvanDam <kathvandam@gmail.com>
2021-03-18 17:15:13 +0100

    dotenv and db

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+const dotenv = require('dotenv').config();
+const { Mongoclient } = require('mongodb');
+
+const dbURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URI}`;
+
+let db;
+
+MongoClient.connect(dbURL, { useUnifiedTopology: true }, (err, client) => {
+	if (err) {
+		console.log('MongoDB Error:' + err);
+	} else {
+		db = client.db(process.env.DB_NAME);
+		console.log('Connectie gelukt');
+	}
+});
 
 app.use(express.static('public'));
 app.set("view engine", "ejs");
