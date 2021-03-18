9c309ffc31a6e54a81bcf848978c94b812c8e51f
KathleenvanDam <kathvandam@gmail.com>
2021-03-18 14:40:24 +0100

    add title to like view

--- index.js
+++ index.js
@@ -3,13 +3,14 @@ const app = express();
 const port = 3000;
 
 app.use(express.static('public'));
+app.set("view engine", "ejs");
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
 });
 
 app.get('/likes', (req, res) => {
-    res.send('Test')
+    res.render('like', {title:'hello'})
 });
 
 app.use(function (req, res, next) {
