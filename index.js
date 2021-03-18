f78499d9d022e7390389c9378124dfd81d894c04
KathleenvanDam <kathvandam@gmail.com>
2021-03-18 14:28:28 +0100

    add routes

--- index.js
+++ index.js
@@ -10,6 +10,10 @@ app.get('/likes', (req, res) => {
     res.send('Test')
 });
 
+app.use(function (req, res, next) {
+	res.status(404).send("Sorry can't find that page")
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
