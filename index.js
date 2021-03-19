57ce8494d9bd05ccaa8694bbb8af9e1d277a4ec3
dannyfrelink <danny-frelink@hotmail.com>
2021-03-19 14:21:36 +0100

    download filereader

--- index.js
+++ index.js
@@ -6,9 +6,10 @@ const port = 5555;
 const countriesList = require('countries-list');
 const countries = Object.values(countriesList.countries);
 const bodyParser = require('body-parser');
-
-// const multer = require('multer');
-
+const multer = require('multer');
+const { v4: uuidv4 } = require('uuid');
+/* eslint-disable-next-line no-unused-vars */
+const FileReader = require('filereader');
 /* eslint-disable-next-line no-unused-vars */
 const dotenv = require('dotenv').config();
 const { MongoClient } = require('mongodb');
@@ -28,7 +29,39 @@ MongoClient.connect(dbURL, { useUnifiedTopology: true }, (err, client) => {
 	}
 });
 
+const options = {
+	random: [
+		0x10,
+		0x91,
+		0x56,
+		0xbe,
+		0xc4,
+		0xfb,
+		0xc1,
+		0xea,
+		0x71,
+		0xb4,
+		0xef,
+		0xe1,
+		0x67,
+		0x1c,
+		0x58,
+		0x36
+	]
+};
 
+const storage = multer.diskStorage({
+	destination: (req, file, callback) => {
+		callback(null, './public/uploads/');
+	},
+	filename: (req, file, callback) => {
+		callback(null, `${uuidv4(options.random)}.jpg`);
+	}
+});
+
+const upload = multer({
+	storage: storage
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
@@ -38,11 +71,13 @@ app.get('/profile', (req, res) => {
 	res.render('profile', {title: 'Profile', countries});
 });
 
-app.post('/profile', async (req, res) => {
+app.post('/profile', upload.single('image'), async (req, res) => {
 	let personalDB;
 
+	const img = `uploads/${req.file.path.split('/').pop()}`;
+
 	try {
-		const document = { 'name': req.body.name, 'countries': req.body.countries, 'gender': req.body.gender, 'birthdate': req.body.date, 'sports': req.body.sports, 'interests': req.body.interests };
+		const document = { 'image': img, 'name': req.body.name, 'countries': req.body.countries, 'gender': req.body.gender, 'birthdate': req.body.date, 'sports': req.body.sports, 'interests': req.body.interests };
 		await personal.insertOne({ document });
 
 		personalDB = await personal.findOne({}, { sort: { _id: -1 }, limit: 1 });
@@ -50,11 +85,7 @@ app.post('/profile', async (req, res) => {
 	catch (error) {
 		console.error('Error:', error);
 	}
-
-
-
-
-	res.render('profileAdded', { personalDB });
+	res.render('profileAdded', { title: 'Profile', personalDB, countries });
 });
 
 
