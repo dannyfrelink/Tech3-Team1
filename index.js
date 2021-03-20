22afa9ef3f569d074b162e377e597e54967a7a39
dannyfrelink <danny-frelink@hotmail.com>
2021-03-20 12:09:28 +0100

    if en else voor app.get

--- index.js
+++ index.js
@@ -67,9 +67,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
-app.get('/profile', (req, res) => {
-	res.render('profile', {title: 'Profile', countries});
-});
+// app.get('/profile', (req, res) => {
+// 	res.render('profile', {title: 'Profile', countries});
+// });
+
 
 app.post('/profile', upload.single('image'), async (req, res) => {
 	let personalDB;
@@ -88,8 +89,17 @@ app.post('/profile', upload.single('image'), async (req, res) => {
 	res.render('profileAdded', { title: 'Profile', personalDB, countries });
 });
 
-
-
+app.get('/profile', async (req, res) => {
+	if(await personal.countDocuments() > 0) {
+		let personalDB;
+		personalDB = await personal.findOne({}, { sort: { _id: -1 }, limit: 1 });
+		res.render('profileAdded', { title: 'Profile', personalDB, countries });
+	}
+	else {
+		console.log(personal.countDocuments());
+		res.render('profile', {title: 'Profile', countries});
+	}
+});
 
 app.use(function (req, res) {
 	res.status(404).send('Sorry, could not find this page.');
