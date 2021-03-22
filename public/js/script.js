0fb25366f97a91e8d9d357009d640119d704ed55
dannyfrelink <danny-frelink@hotmail.com>
2021-03-22 11:31:49 +0100

    disabled en enabled maken van input[type file]

--- public/js/script.js
+++ public/js/script.js
@@ -13,7 +13,6 @@ function previewImage() {
 		const fileReader = new FileReader();
 		fileReader.readAsDataURL(files);
 		fileReader.addEventListener('load', function () {
-			// imgPreview.style.display = 'block';
 			imgPreview.innerHTML = `<img src="${this.result}" />`;
 		});   
 
