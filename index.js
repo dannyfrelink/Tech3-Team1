d4c085a30508aa97c69ab1ce6e1a16820103a7aa
mandemt <70643363+mandemt@users.noreply.github.com>
2021-03-23 15:09:19 +0100

    .env

--- index.js
+++ index.js
@@ -2,6 +2,25 @@ const express = require('express');
 const app = express(); 
 const port = 3000;
 
+let db = null
+async function connectDB(){
+const uri = process.env.DB_URI
+const options = {useUnifiedTopology: true}
+const client = new MongoClient(uri,options);
+await client.connect(); // hierdoor worden geen andere taken uitgevoed totdat er verbonden is.
+db = await client.db(process.env.DB_NAME)
+}
+
+connectDB()
+	.then(() =>{
+	console.log('gelukt om te verbinden met de database in .env bestand')
+	})
+	.catch(error =>{
+		console.log(error)
+	})
+
+
+
 app.set('view engine', 'ejs') // instellen voor view engine
 
 app.use(express.static('static'))
