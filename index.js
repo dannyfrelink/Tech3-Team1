113ad6cab2ec5a3e71fac8268508d9a1f99c0cdb
TristanVarewijck <tristanconnect4@outlook.com>
2021-03-23 13:27:54 +0100

    Co-authored-by: Danny Frelink <dannyfrelink@users.noreply.github.com>

--- index.js
+++ index.js
@@ -40,9 +40,10 @@ app.get('/', async (req, res) => {
 	let filterDB = {}; 
 	filterDB = await filter.findOne({}, { sort: { _id: -1 }, limit: 1 });
 	profiles = await db.collection('profile').findOne({like:false});
+	const profilesLength = await db.collection('profile').countDocuments({like:false})
 	res.render('index', {
 		title: 'Sportbuddy',
-		results: profiles.length,
+		profilesLength,
 		countries,
 		profiles: profiles,
 		filterDB
@@ -53,16 +54,37 @@ app.get('/', async (req, res) => {
 // form method="post"
 app.post('/liked', async (req, res) => {
 	await db.collection('profile').updateOne({'like':false}, {$set:{'like':true}}, {sort: {_id:1}});
+
+	try {
+		await db.collection('profile').updateOne({'like':false}, {$set:{'like':true}}, {sort: {_id:1}});
+		} 
+	catch (error) {
+		console.error('Error:', error);}
+
 	let profiles = {};
 	profiles = await db.collection('profile').find({like:false}).toArray();
 	let filterDB = {}; 
 	filterDB = await filter.findOne({}, { sort: { _id: -1 }, limit: 1 });
 
-	profiles = await db.collection('profile').findOne({like:false});
+	if (req.body.sport !== 'All') {
+		profiles = profiles.filter(profile => { return profile.sport == req.body.sport; });
+	}
+	if (req.body.age !== 'All') {
+		profiles = profiles.filter(profile => { return profile.age <= req.body.age; });
+	}
+	if (req.body.country !== 'All') {
+		profiles = profiles.filter(profile => { return profile.country == req.body.country; });
+	}
+	if (req.body.gender !== 'All') {
+		profiles = profiles.filter(profile => { return profile.gender == req.body.gender; });
+	}
+
+	const profilesLength = await db.collection('profile').countDocuments({})
+	profiles = await profiles[0];
 
 	res.render('index', {
 		title: 'SportBuddy',
-		results: profiles.length,
+		profilesLength,
 		countries, 
 		profiles: profiles,
 		filterDB
@@ -71,7 +93,21 @@ app.post('/liked', async (req, res) => {
 
 app.post('/', async (req, res) => {
 	// data from database
-	let profiles = {};
+	let filterDB;
+		try {
+			const document = { 'sport': req.body.sport, 'age': req.body.age, 'country': req.body.country, 'gender': req.body.gender };
+			filterDB = await filter.findOne({}, { sort: { _id: -1 }, limit: 1 });
+			if(await filter.countDocuments() > 0) {	
+				await filter.updateOne({}, {$set: { document }});
+			} 
+			else {
+			   await filter.insertOne({ document });
+			}
+		    }    
+		catch (error) {
+			console.error('Error:', error);
+		}
+	
 	profiles = await db.collection('profile').find({like:false}).toArray();
 	// filter criteria
 	if (req.body.sport !== 'All') {
@@ -86,28 +122,14 @@ app.post('/', async (req, res) => {
 	if (req.body.gender !== 'All') {
 		profiles = profiles.filter(profile => { return profile.gender == req.body.gender; });
 	}
+	
 
-
-	let filterDB;
-		try {
-			const document = { 'sport': req.body.sport, 'age': req.body.age, 'country': req.body.country, 'gender': req.body.gender };
-			if(await filter.countDocuments() > 0) {
-			   await filter.updateOne({}, {$set: { document }});
-			} 
-			else {
-			   await filter.insertOne({ document });
-			}
-			filterDB = await filter.findOne({}, { sort: { _id: -1 }, limit: 1 });
-		    }    
-		catch (error) {
-			console.error('Error:', error);
-		}
-
-	profiles = await db.collection('profile').findOne({like:false});
-
+	const profilesLength = await db.collection('profile').countDocuments({})
+	profiles = profiles[0];
+	
 	res.render('index', {
 		title: 'SportBuddy',
-		results: profiles.length,
+		profilesLength,
 		countries, 
 		profiles: profiles,
 		filterDB
