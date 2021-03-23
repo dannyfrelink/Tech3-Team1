2554a38b7a1d582e6a47ee1526d7ed663614a25a
mandemt <70643363+mandemt@users.noreply.github.com>
2021-03-23 15:36:09 +0100

    database code verder

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 const dotenv = require('dotenv').config();
 const {MongoClient} = require('mongodb');
-
+const bodyParser = require("body-parser");
 const dbURL = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URI}`;
 
 let db;
@@ -16,8 +16,10 @@ MongoClient.connect(dbURL, { useUnifiedTopology: true }, (err, client) => {
 		console.log('Connectie gelukt');
 	}
 });
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
+// console.log(req.body.name)
 
-console.log(req.body.name)
 
 
 app.set('view engine', 'ejs') // instellen voor view engine
@@ -25,9 +27,13 @@ app.set('view engine', 'ejs') // instellen voor view engine
 app.use(express.static('static'))
 
 app.listen(port,() =>{
-    console.log('luistert op port: ${port}');
+    console.log('luistert op port: port');
 })
 
-app.get('/', (req,res)=>{
-    res.render('index', {title: 'Explore'});
+app.get('/', async (req,res)=>{
+	let users = {}
+	users = db.collection('profile').updateOne({"name":req.body.name}, {$set:{"like":true}});
+	res.render('index', {title: 'explore'});
+	console.log(req.body.name)
+    
 })
