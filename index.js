1a13e5d3dc857c635b97ddbcef9978c38479a482
mandemt <70643363+mandemt@users.noreply.github.com>
2021-03-23 15:21:05 +0100

    database connectie

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express'); 
 const app = express(); 
 const port = 3000;
+const dotenv = require('dotenv').config();
+const {MongoClient} = require('mongodb');
 
 let db = null
 async function connectDB(){
