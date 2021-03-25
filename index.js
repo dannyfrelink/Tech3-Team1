b2897b67f7e4f4386837d4cb6ada5a30ea020a1a
mandemt <70643363+mandemt@users.noreply.github.com>
2021-03-25 15:26:13 +0100

    slider

--- index.js
+++ index.js
@@ -57,3 +57,5 @@ app.use(function (req, res, next) {
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
 });
+
+
--- public/script.js
+++ public/script.js
@@ -1,8 +1,23 @@
 console.log("client side running");
 
-const button = document.getElementById('btn');
-button.addEventListener('click', test);
 
-function test(){
-    console.log('button was clicked');
+
+var switchKnop = document.querySelector('#switch');
+var likeContent = document.querySelector('#likes')
+var matchesContent = document.querySelector('#matches')
+
+function likes () {
+    likeContent.classList.add('like')
+    matchesContent.classList.remove('match')
+
 }
+function matches () {
+    likeContent.classList.remove('like')
+    matchesContent.classList.add('match')
+
+
+}
+
+likeContent.addEventListener('click', likes);
+matchesContent.addEventListener('click', matches);
+
